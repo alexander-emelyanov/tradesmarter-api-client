921ad5314110b48b4ce310b2b1ec366a8787732d
Alexander Emelyanov <alexander.emelyanov@faunusanalytics.com>
2016-02-29 16:28:05 +0100

    TestCase got methods setUp and tearDown().

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,5 +4,17 @@ namespace TradeSmarter\Tests;
 
 class TestCase extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * Sets up a test with some useful objects.
+     */
+    public function setUp()
+    {
+    }
 
+    /**
+     * Free resources
+     */
+    public function tearDown()
+    {
+    }
 }
